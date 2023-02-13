5522e6272bfb8e8b1a6ea46e56da96508a3dae9d
ushabh-jain <112158902+ushabh-jain@users.noreply.github.com>
2023-02-13 09:29:33 +0530

    Add files via upload

---
+++ fourth.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+ int a,b;
+ printf("enter your first number here : \n");
+ scanf("%d" , &a);
+ printf("enter your second number here :  \n");
+ scanf("%d" , &b);
+ printf("answer is : %d \n" , a+b);
+ printf("answer is : %d \n" , a*b );
+ printf("answer is : %d \n" , a-b );
+ printf("answer is : %d \n" , a/b );
+ return 0;
+}
